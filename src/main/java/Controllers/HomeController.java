23f44edf3843b12b97825593fe2b56ace7014126
yassinenaciri <yassinen84@gmail.com>
2022-03-29 10:37:45 +0000

    Added home controller

---
+++ src/main/java/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
