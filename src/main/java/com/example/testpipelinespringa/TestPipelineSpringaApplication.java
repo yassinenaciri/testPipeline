11b706ad9d07dcd95789d4a32296914384001e5f
yassinenaciri <yassinen84@gmail.com>
2022-03-29 10:25:02 +0000

    initialized spring project

---
+++ src/main/java/com/example/testpipelinespringa/TestPipelineSpringaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpipelinespringa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPipelineSpringaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPipelineSpringaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpipelinespringa/TestPipelineSpringaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpipelinespringa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPipelineSpringaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
